70a8c282a0d893e410caa78172176903d61826e4
Mohammadreza <rashidpour1995@gmail.com>
2025-05-04 11:16:51 +0330

    edit ayeh number to 110

--- home.php
+++ home.php
@@ -247,7 +247,7 @@
 <div
     class="zba-ayeha text-primary text-center d-flex flex-column justify-content-center align-items-center h-100 py-5 ">
     <h2 class="fw-900 f-28px"> برای مسابقه کدام آیه ها را باید بخوانم؟ </h2>
-    <p class="f-21px">نهضت ملی "زندگی با آیه ها" ۳۰ آیه قرآن کریم توسط جمع کثیری از اندیشمندان قرآنی کشور انتخاب شده اند
+    <p class="f-21px">نهضت ملی "زندگی با آیه ها" 110 آیه قرآن کریم توسط جمع کثیری از اندیشمندان قرآنی کشور انتخاب شده اند
         که تسلط به آنها می تواند مبانی اعتقادی مخاطبین را مستحکم تر کند. </p>
     <div class="ayeh-swiper overflow-hidden position-relative ">
         <div class="swiper-wrapper">
